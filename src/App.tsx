403ef8e2efaea14d42abfa58913cb5edfe1bed7a
hiroko-ino <los73.butterfly.1251.12.15@gmail.com>
2022-03-20 18:09:53 +0900

    フォントの調整

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import {
   ChakraProvider,
   Box,
   Text,
-  theme,
 } from "@chakra-ui/react"
 import { INDEX_DATA } from "./const"
+import theme from "./theme"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box p="8" minH="100vh" bgGradient='linear(to-br, green.200, pink.200)'>
-      <Text align="center" fontSize="2xl">
+      <Text align="center" fontSize="2xl" fontWeight={600}>
         {INDEX_DATA.SITENAME}
       </Text>
     </Box>
