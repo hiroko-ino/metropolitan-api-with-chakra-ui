29a67e1dc75ec46bb7890ca4d35b1b8f85a43cfe
hiroko-ino <los73.butterfly.1251.12.15@gmail.com>
2022-04-05 21:31:55 +0900

    Updates

--- src/theme.ts
+++
@@ -1,9 +0,0 @@
-import { extendTheme } from "@chakra-ui/react";
-
-const theme = extendTheme({
-    fonts: {
-        body: `"Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif;`,
-    },
-})
-
-export default theme
--- src/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export const getRandom = (arr: number[]) => {
-  const randomNumber = Math.floor(Math.random() * arr.length);
-  return arr[randomNumber];
-}
